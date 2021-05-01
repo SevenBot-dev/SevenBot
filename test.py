53f8421003f66aa9196120ebfbaee728a7989684
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-05-01 12:57:25 +0900

    Code: コードを整形

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 from common_resources.tokens import TOKEN
-bot= commands.Bot(["sbt#", "sbt. ", "sbt."], intents=discord.Intents.all())
+bot = commands.Bot(["sbt#", "sbt. ", "sbt."], intents=discord.Intents.all())
 
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
 bot.load_extension("jishaku")
 bot.run(TOKEN)
