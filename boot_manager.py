9a0867b42fe2411a5671609061c12c882c73dbc0
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-02 05:19:13 +0900

    Change: auto_updaterを強化

--- boot_manager.py
+++ boot_manager.py
@@ -1,15 +1,28 @@
-import time
+import os
 import subprocess
+import sys
+import time
 
-cmd = "python3.9 main.py"
-p = subprocess.Popen("exec " + cmd, shell=True)
+if sys.platform == 'win32':
+    cmd = "python main.py"
+else:
+    cmd = "exec python3.9 main.py"
+p = subprocess.Popen(cmd, shell=True)
 
 while True:
     res = subprocess.run("git pull origin main --dry-run".split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     if "main.py" in res.stdout:
         p.kill()
         res = subprocess.run("git pull origin main".split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        p = subprocess.Popen("exec " + cmd, shell=True)
+        p = subprocess.Popen(cmd, shell=True)
     elif "origin/" in res.stdout:
         subprocess.run("git pull origin main".split(), stdout=subprocess.DEVNULL)
+    elif os.path.exists("./reboot"):
+        os.remove("./reboot")
+        p.kill()
+        p = subprocess.Popen(cmd, shell=True)
+    elif os.path.exists("./shutdown"):
+        os.remove("./shutdown")
+        p.kill()
+        break
     time.sleep(10)
