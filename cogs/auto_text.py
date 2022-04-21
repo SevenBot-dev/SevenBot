8d57ad08f82aaec767330cfa930709ad2921c521
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-04-21 22:19:45 +0900

    Fix: AttributeErrorを修正

--- cogs/auto_text.py
+++ cogs/auto_text.py
@@ -72,7 +72,7 @@ class AutoTextCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_channel_update(
-        self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState
+        self, member: discord.Member, before: discord.VoiceChannel, after: discord.VoiceChannel
     ):
         if before is not None and (not before.members) and before in Auto_text_channels:
             try:
@@ -82,19 +82,19 @@ class AutoTextCog(commands.Cog):
             del Auto_text_channels[before]
         if (
             after is not None
-            and after.channel.id in self.bot.guild_settings[member.guild.id]["auto_text"]
-            and after.channel.id not in Auto_text_channels
+            and after.id in self.bot.guild_settings[member.guild.id]["auto_text"]
+            and after.id not in Auto_text_channels
         ):
             ntc = await after.category.create_text_channel(
                 after.name,
-                topic=f"このチャンネルは{after.channel.mention}に誰もいなくなったら自動的に消去されます。",
+                topic=f"このチャンネルは{after.mention}に誰もいなくなったら自動的に消去されます。",
             )
             Auto_text_channels[after] = ntc
             await ntc.send(
                 member.mention,
                 embed=SEmbed(
                     "",
-                    f"このチャンネルは{after.channel.mention}に誰もいなくなったら自動的に消去されます。",
+                    f"このチャンネルは{after.mention}に誰もいなくなったら自動的に消去されます。",
                     color=Info,
                 ),
             )
